bf8061f368be3d5b166f2d22aef49c961cc4fbbf
miyamoto <miyamoto.chichi@gmail.com>
2020-08-10 15:07:51 +0900

    STEP 5-7

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,11 +4,21 @@ import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 
+import SEO from "../components/seo"
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUtensils, faCheckSquare } from "@fortawesome/free-solid-svg-icons"
 
-export default ({ data }) => (
+export default ({ data, location }) => (
   <Layout>
+    <SEO
+      pagetitle="ESSENSIALについて"
+      pagedesc="食べ物についての情報を発信しているサイトです。"
+      pagepath={location.pathname}
+      pageimg={data.about.childImageSharp.original.src}
+      pageimgw={data.about.childImageSharp.original.width}
+      pageimgh={data.about.childImageSharp.original.height}
+    />
     <div className="eyecatch">
       <figure>
         <Img
@@ -71,6 +81,11 @@ export const query = graphql`
       childImageSharp {
         fluid(maxWidth: 1600) {
           ...GatsbyImageSharpFluid_withWebp
+        }
+        original {
+          src
+          height
+          width
         }
       }
     }
